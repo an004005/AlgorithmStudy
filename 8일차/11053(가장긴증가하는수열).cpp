12d54552710c69a25b919a1adcc9ea3217c79e57
안중환 <an004005@gmail.com>
2022-08-19 20:26:58 +0900

    add LIS

---
+++ 8일차/11053(가장긴증가하는수열).cpp
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N;
+int arr[1001]{};
+int dp[1001]{}; //dp[i]: 길이가 i 인 증가하는 부분 수열 중 끝점의 최솟값
+int len = 0;// 수열 길이
+
+int bisearch(int n)
+{
+	int lower = 0;
+	int upper = len - 1;
+	int ret;
+
+	while (lower <= upper)
+	{
+		int mid = (upper + lower) / 2;
+		if (dp[mid] >= arr[n])
+		{
+			ret = mid;
+			upper = mid - 1;
+		}
+		else
+		{
+			lower = mid + 1;
+		}
+	}
+
+	return ret;
+}
+/*
+6
+3 2 5 2 3 1
+*/
+
+int main()
+{
+	cin >> N;
+	for (int i = 0; i < N; ++i)
+		cin >> arr[i];
+	
+	for (int i = 0; i < N; ++i)
+	{
+		if (arr[i] > dp[len - 1])
+		{
+			dp[len] = arr[i]; // 마지막 배열값보다 크다. 못찾기 때문에 새로 추가
+			++len;
+			continue;
+		}
+
+		dp[bisearch(i)] = arr[i]; // 최소값 갱신
+	}
+
+	cout << len << endl;
+
+
+	return 0;
+}
